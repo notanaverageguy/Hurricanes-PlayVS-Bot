c1b3fd231542cdea18043831ce3c395f272bd26b
orosado844@gmail.com <83960401+notanaverageguy@users.noreply.github.com>
2023-09-28 22:21:49 -0400

    first command set up

---
+++ commands/ping.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Test command'),
+		
+	args: [],
+    permissions: [],
+
+	async execute(interaction) { 
+        interaction.reply('pong')
+	},
+};
