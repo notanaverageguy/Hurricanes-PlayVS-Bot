6e054d7c3b8490b3a8cc222368d0f4697d0193fc
orosado844@gmail.com <83960401+notanaverageguy@users.noreply.github.com>
2023-09-29 18:45:57 -0400

    update database.js

--- libs/database.js
+++ libs/database.js
@@ -1,11 +1,61 @@
-const PocketBase = require('pocketbase/cjs')
+const PocketBase = require("pocketbase/cjs");
+const db = new PocketBase("http://127.0.0.1:8090");
 
-  
+async function calcPlayerWins(id) {
+	const games = await db
+		.collection("Games")
+		.getFullList({
+			filter: `players.id ?~ "${id}"`,
+		})
+		.catch(() => {
+			return console.log("couldnt find games");
+		});
 
-function recalcPlayerStats(name) {
+	const data = {
+		games_played: 0,
+		games_won: 0,
+		games_lost: 0,
+	};
+
+	for (const game of games) {
+		data.games_played++;
+		if (game.won) {
+			data.games_won++;
+		} else {
+			data.games_lost++;
+		}
+	}
+	await db.collection("Players").update(id, data);
+}
+
+async function calcGameScore(id) {
+	const rounds = await db
+		.collection("Rounds")
+		.getFullList({
+			filter: `game.id = "${id}"`,
+		})
+		.catch(() => {
+			return console.log("couldnt find games");
+		});
     
+    const data = {
+        wins: 0,
+        losses: 0,
+    }
+
+    for(const round of rounds) {
+        if(round.won) {
+            data.wins ++;
+        } else {
+            data.losses ++;
+        }
+    }
+
+    await db.collection("Games").update(id, {score: `${data.wins} - ${data.losses}`});
 }
 
 module.exports = {
-    db: new PocketBase('http://127.0.0.1:8090')
-}
+	db: db,
+    calcGameScore: calcGameScore,
+    calcPlayerWins: calcPlayerWins,
+};
