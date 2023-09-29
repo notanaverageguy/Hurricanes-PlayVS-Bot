08877fade5f9e48dc86bc3e7362753c550d71db3
orosado844@gmail.com <83960401+notanaverageguy@users.noreply.github.com>
2023-09-29 05:50:04 -0400

    create 1 file and update 2 files

--- commands/getPlayers.js
+++ commands/getPlayers.js
@@ -11,15 +11,14 @@ module.exports = {
   permissions: [],
 
   async execute(interaction) {
-	var playerList = await db.collection("Players").getFullList()
-
+	var playerList = await db.collection("Players").getFullList();
 	const exampleEmbed = new EmbedBuilder()
 	.setColor(0x0099FF)
 	.setTitle('Players on the Hurricanes Team')
 	.setAuthor({ name: 'Bot made by Naag', iconURL: 'https://cdn.discordapp.com/avatars/952239410055888916/48e9b5fcc52babe9ad6e68d49dad124c.webp', url: 'https://discord.js.org' })
 
 	for(const player of playerList) {
-		exampleEmbed.addFields({ name: `${player.name}`, value: `${player.role}`, inline: true })
+		exampleEmbed.addFields({ name: `${player.name}`, value: `${player.role}`, inline: true });
 	}
 
     interaction.reply({ embeds: [exampleEmbed] });
