51d525c26988121c4f40117310abd1a6fa94951c
orosado844@gmail.com <83960401+notanaverageguy@users.noreply.github.com>
2023-09-30 13:04:15 -0400

    update game.js and player.js

--- commands/game related/game.js
+++ commands/game related/game.js
@@ -9,8 +9,8 @@ module.exports = {
 		.setDescription("Gets stats of a game")
 		.addStringOption((option) =>
 			option
-				.setName("id")
-				.setDescription("ID of the game")
+				.setName("search")
+				.setDescription("ID OR opponent of the game")
 				.setRequired(true)
 		),
 
@@ -18,71 +18,86 @@ module.exports = {
 	permissions: [],
 
 	async execute(interaction) {
-		const idSearch = interaction.options.getString("id");
-		var game = await db
-			.collection("Games")
-			.getOne(idSearch)
-			.catch(() => {
-				return null;
-			});
-		if (!game)
-			return interaction.reply(`No game found with ID '**${idSearch}**'`);
+		const search = interaction.options.getString("search");
+		const games = await db.collection("Games").getFullList({
+			filter: `id='${search}'||opponent='${search.toLowerCase()}'`,
+		});
 
-		game = await calcGameScore(game.id);
-		const team = await db.collection("Teams").getOne(game.team);
-		const embed = new EmbedBuilder()
-			.setColor(0x0099ff)
-			.setTitle(`vs. ${upperCaseEveryWord(game.opponent)}`)
-			.setAuthor({
-				name: "Bot made by Naag",
-				iconURL:
-					"https://cdn.discordapp.com/avatars/952239410055888916/48e9b5fcc52babe9ad6e68d49dad124c.webp",
-				url: "https://discord.js.org",
-			})
-			.addFields(
-				{ name: "Team", value: team.name, inline: true },
-				{ name: "Score", value: game.score, inline: true },
-				{
-					name: "Date",
-					value: game.played.split(" ")[0],
+		if (!games.length)
+			return interaction.reply(
+				`No game found with search '**${search}**'`
+			);
+
+		const embeds = [];
+		for (var game of games) {
+			game = await calcGameScore(game.id);
+			const team = await db.collection("Teams").getOne(game.team);
+			const embed = new EmbedBuilder()
+				.setColor(0x0099ff)
+				.setTitle(`vs. ${upperCaseEveryWord(game.opponent)}`)
+				.setAuthor({
+					name: "Bot made by Naag",
+					iconURL:
+						"https://cdn.discordapp.com/avatars/952239410055888916/48e9b5fcc52babe9ad6e68d49dad124c.webp",
+					url: "https://discord.js.org",
+				})
+				.addFields(
+					{ name: "Team", value: team.name, inline: true },
+					{ name: "Score", value: game.score, inline: true },
+					{
+						name: "Date",
+						value: game.played.split(" ")[0],
+						inline: true,
+					}
+				)
+				.setFooter({
+					text: `Game id: ${game.id}`,
+				});
+
+			// Getting rounds
+			const rounds = await db.collection("Rounds").getFullList({
+				filter: `game.id = "${game.id}"`,
+			});
+
+			for (const index in rounds) {
+				const round = rounds[index];
+
+				embed.addFields({
+					name: `Round ${round.round}`,
+					value: `\`ID: ${round.id}\`\n**Score:** *${
+						round.score
+					}*\n**Outcome:** *${round.won ? "won" : "loss"}*`,
 					inline: true,
-				}
-			)
-			.setFooter({
-				text: `Game id: ${game.id}`,
-			});
-
-		// Getting rounds
-		const rounds = await db.collection("Rounds").getFullList({
-			filter: `game.id = "${game.id}"`,
-		});
-
-		for (const index in rounds) {
-			const round = rounds[index];
+				});
+			}
 
 			embed.addFields({
-				name: `Round ${round.round}`,
-				value: `\`ID: ${round.id}\`\n**Score:** *${round.score}*\n**Outcome:** *${round.won ? "won" : "loss"}*`,
-				inline: true,
+				name: "\u200B",
+				value: "\u200B",
+				inline: false,
 			});
+
+			// Getting players
+			for (var player of game.players) {
+				player = await db.collection("Players").getOne(player);
+				embed.addFields({
+					name: `**${upperCaseEveryWord(player.first_name)}**`,
+					value: `${player.role}`,
+					inline: true,
+				});
+			}
+
+			embeds.push(embed);
 		}
 
-		embed.addFields({
-			name: "\u200B",
-			value: "\u200B",
-			inline: false,
+		interaction.reply({
+			content: `${
+				embeds.length == 1
+					? ""
+					: `Found ${embeds.length} games using search ${search}`
+			}`,
+			embeds: embeds,
+			ephemeral: true,
 		});
-
-		// Getting players
-		for (var player of game.players) {
-			player = await db.collection("Players").getOne(player);
-			embed.addFields({
-				name: `**${upperCaseEveryWord(player.first_name)}**`,
-				value: `${player.role}`,
-				inline: true,
-			});
-		}
-
-		interaction.reply({ embeds: [embed] });
 	},
 };
--- commands/player related/player.js
+++ commands/player related/player.js
@@ -20,44 +20,71 @@ module.exports = {
 
 	async execute(interaction) {
 		const search = interaction.options.getString("search");
-		var player = await findPlayer(search);
-		if (!player) return interaction.reply(`Invalid player`);
+		const players = await db.collection("Players").getFullList({
+			filter: `id = "${search}" || first_name = "${search}" || last_name = "${search}" || (first_name = "${
+				search.split(" ")[0]
+			}" && last_name = "${search.split(" ")[1]}")`,
+		});
 
-		player = await calcPlayerWins(player.id);
-		const team = await db.collection("Teams").getOne(player.team);
-		const embed = new EmbedBuilder()
-			.setColor(0x0099ff)
-			.setTitle(`Stats for ${player.first_name}`)
-			.setAuthor({
-				name: "Bot made by Naag",
-				iconURL:
-					"https://cdn.discordapp.com/avatars/952239410055888916/48e9b5fcc52babe9ad6e68d49dad124c.webp",
-				url: "https://discord.js.org",
-			})
-			.addFields(
-				{ name: "Team", value: team.name, inline: true },
-				{ name: "Role", value: player.role, inline: true },
-				{ name: "\u200B", value: "\u200B", inline: true },
-				{
-					name: "Games Played",
-					value: `${player.games_played}`,
-					inline: true,
-				},
-				{
-					name: "Games Won",
-					value: `${player.games_won}`,
-					inline: true,
-				},
-				{
-					name: "Games Lost",
-					value: `${player.games_lost}`,
-					inline: true,
-				}
-			)
-			.setFooter({
-				text: `Player id: ${player.id}`,
+		if (!players.length)
+			return interaction.reply({
+				content: `No player found with search ${search}`,
+				ephemeral: true,
 			});
 
-		interaction.reply({ embeds: [embed] });
+		const embeds = [];
+		for (var player of players) {
+			player = await calcPlayerWins(player.id);
+			const team = await db.collection("Teams").getOne(player.team);
+			const embed = new EmbedBuilder()
+				.setColor(0x0099ff)
+				.setTitle(`Stats for ${player.first_name}`)
+				.setAuthor({
+					name: "Bot made by Naag",
+					iconURL:
+						"https://cdn.discordapp.com/avatars/952239410055888916/48e9b5fcc52babe9ad6e68d49dad124c.webp",
+					url: "https://discord.js.org",
+				})
+				.addFields(
+					{ name: "Team", value: team.name, inline: true },
+					{ name: "Role", value: player.role, inline: true },
+					{ name: "\u200B", value: "\u200B", inline: true },
+					{
+						name: "Games Played",
+						value: `${player.games_played}`,
+						inline: true,
+					},
+					{
+						name: "Games Won",
+						value: `${player.games_won}`,
+						inline: true,
+					},
+					{
+						name: "Games Lost",
+						value: `${player.games_lost}`,
+						inline: true,
+					}
+				)
+				.setFooter({
+					text: `Player id: ${player.id}`,
+				});
+			embeds.push(embed);
+		}
+
+		if (embeds.length > 10)
+			interaction.reply({
+				content: `Too many people found with search ${search}`,
+				ephemeral: true,
+			});
+
+		interaction.reply({
+			content: `${
+				embeds.length == 1
+					? ""
+					: `Found ${embeds.length} players using search ${search}`
+			}`,
+			embeds: embeds,
+			ephemeral: true,
+		});
 	},
 };
